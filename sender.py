76defdf529d4aaf284b1fcc7d22431e68205c7aa
YoiSenshu <101147494+YoiSenshu@users.noreply.github.com>
2025-07-09 17:40:01 +0200

    add a config file, load server data from config

--- sender.py
+++ sender.py
@@ -5,20 +5,22 @@ import random
 FIFO = "image_pipe"
 IMAGE_FOLDER = "images"
 
-# Wysyła po kolei obrazki z folderu w pętli do podanej rury
-
 def send_images():
-    with open(FIFO, "wb") as pipe:
-        while True:
-            for filename in sorted(os.listdir(IMAGE_FOLDER)):
-                if filename.endswith(".png"):
-                    path = os.path.join(IMAGE_FOLDER, filename)
-                    with open(path, "rb") as img:
-                        data = img.read()
-                        size = len(data).to_bytes(4, byteorder="big")
-                        pipe.write(size + data)
-                        pipe.flush()
-                        time.sleep(random.randint(1, 5))
+    while True:
+        try:
+            with open(FIFO, "wb") as pipe:
+                while True:
+                    for filename in sorted(os.listdir(IMAGE_FOLDER)):
+                        if filename.endswith(".png"):
+                            path = os.path.join(IMAGE_FOLDER, filename)
+                            with open(path, "rb") as img:
+                                data = img.read()
+                                size = len(data).to_bytes(4, byteorder="big")
+                                pipe.write(size + data)
+                                pipe.flush()
+                                time.sleep(random.randint(1, 5))
+        except Exception as e:
+            print(e)
 
 if __name__ == "__main__":
     send_images()
--- server.py
+++ server.py
@@ -3,73 +3,76 @@ import threading
 import struct
 import time
 import io
+import yaml
+
+
+with open("config.yml", "r") as f:
+    config = yaml.safe_load(f)
+
+server_host = config["server"]["host"]
+server_port = config["server"]["port"]
+pipe_image_path = config["pipe"]["image"]["path"]
+pipe_tire_settings_path = config["pipe"]["tire-settings"]["path"]
+
 
 app = Flask(__name__)
 
-FIFO = "image_pipe"
 latest_image = None
 lock = threading.Lock()
-
 clients = []
 
 
-def read_exact(pipe, size):
+def read_pipe_bytes(pipe, size):
     buf = b""
     while len(buf) < size:
         chunk = pipe.read(size - len(buf))
         if not chunk:
-            # koniec danych (EOF) — zwróć None, by sygnalizować zakończenie czytania
-            return None
+            return None # end of a file (incorrect length)
         buf += chunk
     return buf
 
 
-def read_pipe():
+def read_image_pipe():
     global latest_image
     while True:
         try:
-            with open(FIFO, "rb") as pipe:
+            with open(pipe_image_path, "rb") as pipe:
                 while True:
-                    size_bytes = read_exact(pipe, 4)
+                    size_bytes = read_pipe_bytes(pipe, 4)
                     if size_bytes is None:
-                        # EOF - piszący zamknął FIFO, przejdź do ponownego otwarcia
                         break
                     size = struct.unpack(">I", size_bytes)[0]
 
-                    data = read_exact(pipe, size)
+                    data = read_pipe_bytes(pipe, size)
                     if data is None:
-                        # EOF - przerwij czytanie i ponownie otwórz FIFO
                         break
 
                     with lock:
                         latest_image = data
-                    print(f"Odebrano obrazek: {size} bajtów")
+                    print(f"Image received: {size} bytes")
         except Exception as e:
-            print(f"Błąd przy czytaniu z rury: {e}")
-
-        # krótka przerwa zanim spróbujemy otworzyć FIFO ponownie
-        time.sleep(0.5)
+            print(f"Error while reading image pipe: {e}")
+        time.sleep(0.5) # wait before next read
 
 
 def event_stream():
-    """Generator do SSE, który wysyła event, gdy pojawi się nowe zdjęcie."""
     last_sent = None
     while True:
-        time.sleep(0.5)
+        time.sleep(0.5) # update frequency
         with lock:
             current = latest_image
         if current is not None and current != last_sent:
             last_sent = current
-            yield f"data: nowy_obrazek\n\n"
+            yield f"new image available\n\n"
 
 
 @app.route("/image")
 def image():
     with lock:
         if latest_image is None:
-            return "Brak obrazka", 404
+            return "", 404
         return send_file(
-            io.BytesIO(latest_image),
+            path_or_file=io.BytesIO(latest_image),
             mimetype="image/png",
             as_attachment=False,
             download_name="image.png",
@@ -81,7 +84,11 @@ def update():
     return Response(event_stream(), mimetype="text/event-stream")
 
 
+@app.route("/tire-settings", methods=["POST"])
+def tire_settings():
+    return "ok", 200
+
+
 if __name__ == "__main__":
-    threading.Thread(target=read_pipe, daemon=True).start()
-    # app.run(host="0.0.0.0", port=2323, threaded=True, ssl_context=('cert.pem', 'key.pem'))
-    app.run(host="0.0.0.0", port=8080, threaded=True)
+    threading.Thread(target=read_image_pipe, daemon=True).start()
+    app.run(host=server_host, port=server_port, threaded=True)
